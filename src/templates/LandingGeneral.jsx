fd5722e5340890dcb7db1fd1d1f1f8f0935f8202
Álvaro <152669823+Perez-Alvaro@users.noreply.github.com>
2025-07-15 16:55:36 -0300

    Add navigation, testimonials and final call components

--- src/templates/LandingGeneral.jsx
+++ src/templates/LandingGeneral.jsx
@@ -4,15 +4,21 @@ import Features from "../components/Features";
 import Steps from "../components/Steps";
 import FAQ from "../components/FAQ";
 import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Testimonials from "../components/Testimonials";
+import FinalSection from "../components/FinalSection";
 
 export default function LandingGeneral({ data }) {
   return (
     <div>
+      <Navbar data={data} />
       <Hero data={data} />
       <About data={data} />
       <Features data={data} />
       <Steps data={data} />
+      <Testimonials data={data} />
       <FAQ data={data} />
+      <FinalSection data={data} />
       <Footer data={data} />
     </div>
   );
